0b94018fd9837f49f19459e60c36113c409bdac9
OmarJeghale <omarjeghalef05@gmail.com>
2025-07-12 20:19:27 -0400

    Handle mood form submission and store in memory

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 # Initialize Flask app w/ folders
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+
+# Store moods in memory
+mood_log = []
 
 # Homepage route
 @app.route('/', methods=['GET', 'POST'])
@@ -9,10 +13,19 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        return ''
-    else:
-        return ''
+        name = request.form['name'].title()
+        mood = request.form['mood']
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
         
+        entry = {
+            'name' : name,
+            'mood' : mood,
+            'timestamp' : timestamp
+        }
+        
+        mood_log.append(entry)
+        
+    return render_template('index.html', mood_log=mood_log)
 
 # Run app
 if __name__ == '__main__':
